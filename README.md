c29db1ad9b47db50b3019b491c4a9a01179eeea1
Warrenww <warrenpig@gmail.com>
2020-09-12 00:48:25 +0800

    Refactor agentReset function && Support inconsist scan direction && Consist the scan and build position

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ This web app is for the purpose to remote control the code bot in Minecraft Educ
 1. Clone this project.
 2. Run `npm install`.
 3. Run `npm start`.
+4. If dashboard does not show automatically, it can be opened at [localhost:3000](http://localhost:3000)
 
 ### Feature
 
@@ -13,3 +14,15 @@ Inspect the block at the [direction] of the bot in game, return the `block name`
 
 #### Inspect Data
 Inspect the block data at the [direction] of the bot in game, return the `int` `block data`.
+
+#### Scan Structure in Game
+Scan the structure from front left of player position to the specific size `[sizeX] * [sizeY] * [sizeZ]`.
+Note that player should stand at the position with smallest x and z in-game coordinate value.
+Once the scan process is done, a link to download the stringify array in `json` format will show.
+
+The performance is about 1 sec/block and a little bit quickly if the block above is `air`. Depending on your network speed.
+
+#### Build Scanned Structure
+Load the `json` file of the scanned structure, and choose the [rotate] value. Then the structure will be reconstruct in the game. Just like scanning function, it will reconstruct the structure from the front left of player position. Note that even the structure is rotated, the point with minimum (x, z) value will always at the front left of player, as shown in below. 
+
+![image](./rotation-example.png)  
--- src/API.js
+++ src/API.js
@@ -11,31 +11,28 @@ const sleep = (ms) => new Promise(function(resolve, reject) {
 const resetAgentPosition = (args = {}) => {
   const {offsetX, offsetY, offsetZ, facingX, facingZ} = args;
   return new Promise(async function(resolve, reject) {
-    await fetch(`http://localhost:8080/executeasother?origin=@p&position=~ ~ ~&command=tp @c 0 0 0 facing ${facingX || 0} 0 ${facingZ || 1}`);
-    await fetch(`http://localhost:8080/executeasother?origin=@p&position=~ ~ ~&command=tp @c ~${offsetX || 0} ~${offsetY || 0} ~${offsetZ || 0}`);
+    await fetch(`http://localhost:8080/executeasother?origin=@p&position=~ ~ ~&command=tp @c ~${offsetX || 0} ~${offsetY || 0} ~${offsetZ || 0} facing ~${(offsetX || 0) + (facingX || 0) * 2} ~ ~${(offsetZ || 0) + (facingZ || 0) * 2}`);
     resolve()
   });
 }
 
 const scan = async (args, callback) => {
-  await resetAgentPosition();
+  await resetAgentPosition({offsetX: 1, offsetY: -1, offsetZ: 1, facingZ: 1});
   const blocks = [];
-  const n = args.Size;
+  const [sizeX, sizeY, sizeZ] = [args.SizeX, args.SizeY, args.SizeZ];
   let flag_x = 1;
   let flag_z = 1;
   console.time("scan");
-  for(let y = 0 ; y < n; y ++){
-    for(let z = 0 ; z < n; z ++){
+  for(let y = 0 ; y < sizeY; y ++){
+    for(let z = 0 ; z < sizeZ; z ++){
       z && await fetch(`http://localhost:8080/move?direction=${flag_z?'forward' : 'back'}`);
-      // z && await fetch(`http://localhost:8080/tptargettopos?victim=@p&destination=~ ~ ~${flag_z ? 1 : -1}`) && await fetch('http://localhost:8080/tptoplayer');
-      for(let x = 0 ; x < n; x ++){
+      for(let x = 0 ; x < sizeX; x ++){
         x && await fetch(`http://localhost:8080/move?direction=${flag_x?'left':'right'}`);
-        // x && await fetch(`http://localhost:8080/tptargettopos?victim=@p&destination=~${flag_x ? 1 : -1} ~ ~`) && await fetch('http://localhost:8080/tptoplayer');
 
-        const block = await fetch("http://localhost:8080/inspect?direction=down").then(res => res.json()).then(result => result);
+        const block = await fetch("http://localhost:8080/inspect?direction=up").then(res => res.json()).then(result => result);
         callback(
           <Box position="relative" display="inline-flex">
-            <CircularProgress variant="static" value={(y * n * n + z * n + x) / n**3 * 100}/>
+            <CircularProgress variant="static" value={(y * sizeZ * sizeX + z * sizeX + x) / (sizeX * sizeY * sizeZ) * 100}/>
             <Box
               top={0}
               left={0}
@@ -46,30 +43,30 @@ const scan = async (args, callback) => {
               alignItems="center"
               justifyContent="center"
             >
-              {`${Math.round( (y * n * n + z * n + x) / n**3 * 100 )}%`}
+              {`${Math.round((y * sizeZ * sizeX + z * sizeX + x) / (sizeX * sizeY * sizeZ) * 100 )}%`}
             </Box>
           </Box>
         );
         if(block.blockName === 'air') continue;
-        const data = await fetch("http://localhost:8080/inspectdata?direction=down").then(res => res.json()).then(result => result);
-        await fetch(`http://localhost:8080/executeasother?origin=@p&position=~ ~ ~&command=execute @c ~~~ setblock ~~-1~ air`);
+        const data = await fetch("http://localhost:8080/inspectdata?direction=up").then(res => res.json()).then(result => result);
+        await fetch(`http://localhost:8080/executeasother?origin=@p&position=~ ~ ~&command=execute @c ~~~ setblock ~~1~ air`);
+        await fetch(`http://localhost:8080/executeasother?origin=@p&position=~ ~ ~&command=execute @c ~~~ fill ~-1~~-1 ~1~~1 air`);
 
         blocks.push({
-          x: flag_x ? x : n - x - 1,
-          y: n - y - 1,
-          z: flag_z ? z : n - z - 1,
+          x: flag_x ? x : sizeX - x - 1,
+          y: sizeY - y - 1,
+          z: flag_z ? z : sizeZ - z - 1,
           block: block.blockName,
           data: data.data,
         });
       }
       flag_x ^= 1;
     }
-    await fetch("http://localhost:8080/move?direction=down");
-    // await fetch(`http://localhost:8080/tptargettopos?victim=@p&destination=~ ~-1 ~`) && await fetch('http://localhost:8080/tptoplayer');
+    await fetch("http://localhost:8080/move?direction=up");
 
     flag_z ^= 1;
   }
-  console.table(blocks.length < 50 && blocks);
+  blocks.length < 50 && console.table( blocks);
   console.timeEnd("scan");
   const downloadURL = await URL.createObjectURL(new Blob([JSON.stringify(blocks)], {type : 'application/json'}));
   callback(
@@ -78,123 +75,78 @@ const scan = async (args, callback) => {
     </Button>
   );
 }
-const __scan = async (args, callback) => {
-  await sleep(1000)
-  await resetAgentPosition({offsetX: 1});
-  const blocks = [];
-  const n = args.Size;
-  let flag_x = 1;
-  let flag_z = 1;
-
-  for(let y = 0 ; y <= n; y ++){
-    for(let x = 0 ; x <= n; x ++){
-      for(let z = 0 ; z <= n; z ++){
-        const success = await fetch(`http://localhost:8080/move?direction=forward`).then(res => res.json()).then(result => result.success);
-        if(!success) {
-          const block = await fetch("http://localhost:8080/inspect?direction=forward").then(res => res.json()).then(result => result);
-          const data = await fetch("http://localhost:8080/inspectdata?direction=forward").then(res => res.json()).then(result => result);
-          await fetch(`http://localhost:8080/executeasother?origin=@p&position=~ ~ ~&command=execute @c ~~~ setblock ~~~1 air`);
-          blocks.push({
-            x: flag_x ? x : n - x - 1,
-            y: n - y - 1,
-            z: flag_z ? z : n - z - 1,
-            block: block.blockName,
-            data: data.data,
-          });
-          z --;
-        }
-      }
-      await fetch("http://localhost:8080/turn?direction=right")
-      await fetch("http://localhost:8080/turn?direction=right")
-      await fetch(`http://localhost:8080/move?direction=${flag_z ? 'right' : 'left'}`)
-      await sleep(100)
-      flag_z ^= 1;
-    }
-    await fetch("http://localhost:8080/move?direction=up")
-    await fetch(`http://localhost:8080/move?direction=${flag_x ? 'right' : 'left'}`)
-    await fetch("http://localhost:8080/turn?direction=right")
-    await fetch("http://localhost:8080/turn?direction=right")
-    flag_x ^= 1;
-  }
-  console.table(blocks);
-  const downloadURL = await URL.createObjectURL(new Blob([JSON.stringify(blocks)], {type : 'application/json'}));
-  callback(
-    <Button variant="contained">
-      <a href={downloadURL} download>download</a>
-    </Button>
-  );
-}
-
-const readFile = (file) => {
-  return new Promise(function(resolve, reject) {
-    const reader = new FileReader();
-    reader.onload = function(evt) {
-      resolve(evt.target.result);
-    };
-    reader.readAsText(file);
-  });
-}
-
-const rotate = (recipe, n = 1, deg = 0) => {
-  if(deg === 90) {
-    recipe.map((item) => {
-      [item.x, item.z] = [-item.z, item.x];
-      item.x += n - 1;
-    });
-  }
-  if(deg === 180) {
-    recipe.map((item) => {
-      [item.x, item.z] = [-item.z, -item.x];
-      item.x += n - 1;
-      item.z += n - 1;
-    });
-  }
-  if(deg === 270) {
-    recipe.map((item) => {
-      [item.x, item.z] = [item.z, -item.x];
-      item.z += n - 1;
-    });
-  }
-
-  return recipe;
-}
-
-const slowFetch = (recipe, callback) => {
-  return new Promise(async function(resolve, reject) {
-    for (var i = 0; i < recipe.length; i++) {
-      // console.log(recipe[i]);
-      await fetch(`http://localhost:8080/executeasother?origin=@p&position=~ ~ ~&command=execute @c ~~~ setblock ~${recipe[i].x}~${recipe[i].y}~${recipe[i].z} ${recipe[i].block} ${recipe[i].data || 0}`);
-      callback(
-        <Box position="relative" display="inline-flex">
-        <CircularProgress variant="static" value={i / recipe.length * 100}/>
-        <Box
-        top={0}
-        left={0}
-        bottom={0}
-        right={0}
-        position="absolute"
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        >
-        {`${Math.round( i / recipe.length * 100 )}%`}
-        </Box>
-        </Box>
-      );
-      if(i % 100 === 0) {
-        await sleep(500);
-        // console.clear();
-      }
-    }
-    resolve();
-  });
-}
 
 const build = async (args, callback) => {
-  await resetAgentPosition();
+  const readFile = (file) => {
+    return new Promise(function(resolve, reject) {
+      const reader = new FileReader();
+      reader.onload = function(evt) {
+        resolve(evt.target.result);
+      };
+      reader.readAsText(file);
+    });
+  }
+
+  const rotate = (recipe, deg = 0, x = 1, z = 1) => {
+    if(deg === 90) {
+      recipe.map((item) => {
+        [item.x, item.z] = [-item.z, item.x];
+        item.x += z;
+      });
+    }
+    if(deg === 180) {
+      recipe.map((item) => {
+        [item.x, item.z] = [-item.x, -item.z];
+        item.x += x;
+        item.z += z;
+      });
+    }
+    if(deg === 270) {
+      recipe.map((item) => {
+        [item.x, item.z] = [item.z, -item.x];
+        item.z += x;
+      });
+    }
+
+    return recipe;
+  }
+
+  const slowFetch = (recipe, callback) => {
+    return new Promise(async function(resolve, reject) {
+      for (var i = 0; i < recipe.length; i++) {
+        // console.log(recipe[i]);
+        await fetch(`http://localhost:8080/executeasother?origin=@p&position=~ ~ ~&command=execute @c ~~~ setblock ~${recipe[i].x}~${recipe[i].y}~${recipe[i].z} ${recipe[i].block} ${recipe[i].data || 0}`);
+        callback(
+          <Box position="relative" display="inline-flex">
+          <CircularProgress variant="static" value={i / recipe.length * 100}/>
+          <Box
+          top={0}
+          left={0}
+          bottom={0}
+          right={0}
+          position="absolute"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          >
+          {`${Math.round( i / recipe.length * 100 )}%`}
+          </Box>
+          </Box>
+        );
+        if(i % 100 === 0) {
+          await sleep(500);
+          // console.clear();
+        }
+      }
+      resolve();
+    });
+  }
+
+  await resetAgentPosition({offsetX: 1, offsetZ: 1, facingZ: 1});
   let recipe = JSON.parse(await readFile(args.File).then(res => res));
+
   console.time("Build");
-  recipe = rotate(recipe, Math.max(...recipe.map(x=>x.x)) + 1, Number(args.Rotate));
+  recipe = rotate(recipe, Number(args.Rotate),  Math.max(...recipe.map(x=>x.x)),  Math.max(...recipe.map(x=>x.z)));
   if(recipe.length > 500) await slowFetch(recipe, callback);
   else {
     recipe.forEach(async (item, index) => {
@@ -250,7 +202,6 @@ const agentInspectData = (args, callback) => {
        }
      )
 }
-
 
 export {
   agentInspectBlock,
